dbfeee4e7b534229ab8245630b4ac01af71aa37c
govindaraj <govindarajk8092@gmail.com>
2025-08-06 13:01:05 +0530

    frontend changes

--- src/app/leaves/leave-request/leave-request/permission-request/permission-request/permission-request.ts
+++ src/app/leaves/leave-request/leave-request/permission-request/permission-request/permission-request.ts
@@ -7,6 +7,9 @@ import { FloatLabelModule } from 'primeng/floatlabel';
 import { TableModule } from 'primeng/table';
 import { CommonModule } from '@angular/common';
 import { DatePicker } from 'primeng/datepicker';
+import { label } from '@primeuix/themes/aura/metergroup';
+import { value } from '@primeuix/themes/aura/knob';
+import { TooltipModule } from 'primeng/tooltip';
 
 interface requestTable {
   empName:string;
@@ -17,15 +20,28 @@ interface requestTable {
   noOfHours:string;
   email:string;
   empId:string;
+  [key:string]:any
 }
 
 @Component({
   selector: 'app-permission-request',
-  imports: [InputIconModule, IconFieldModule, InputTextModule, FloatLabelModule, FormsModule, TableModule, CommonModule],
+  imports: [InputIconModule, IconFieldModule, InputTextModule, FloatLabelModule, FormsModule, TableModule, CommonModule,TooltipModule],
   templateUrl: './permission-request.html',
   styleUrl: './permission-request.css'
 })
 export class PermissionRequest {
+
+  filterReuqusetData:any[] =[];
+  selectedFilter:any = null;
+  filterDropdown:boolean = false;
+
+  filterOption = [
+    {label:'Employee ID', value:'empId'},
+    {label:'Name', value:'name'},
+    {label:'Departmnent', value:'department'},
+    {label:'JobTitle', value:'jobtitle'},
+  ]
+
  requestData :requestTable[] = [
     {
       empName:'Govindaraj', empId:'IM003',department:'Design',jobTitle:'UI/UX Designer',premDate:'09-05-2025',reson:'Personal I don’t w...',noOfHours:'2.15 Hours',email:'govindaraj@gmail.com'
@@ -55,6 +71,48 @@ export class PermissionRequest {
       empName:'Govindaraj', empId:'IM003',department:'Design',jobTitle:'UI/UX Designer',premDate:'09-05-2025',reson:'Personal I don’t w...',noOfHours:'1.38 Hours',email:'govindaraj@gmail.com'
     },
   ]
+
+
+  ngOnInit(){
+    this.requestData = [...this.requestData];
+    this.filterReuqusetData = [...this.requestData];
+  }
+
+  onSearch(event: Event){
+    const input = event.target as HTMLInputElement;
+    const searchText = input.value.trim().toLowerCase();
+
+    if(!searchText){
+      this.filterReuqusetData = [...this.requestData]
+      return
+    }
+
+    const filterKey = this.selectedFilter?.value as keyof requestTable;
+
+    this.filterReuqusetData = this.requestData.filter((request : requestTable)=>{
+      if(filterKey === 'name'){
+        return request.empName?.toLocaleLowerCase().includes(searchText)
+      }
+
+      return request[filterKey]?.toString().toLowerCase().includes(searchText)
+    })
+
+  }
+
+
+  onFilterChange(){
+    this.filterReuqusetData = [...this.requestData]
+  }
+
+  toggleDropdown(){
+    this.filterDropdown =!this.filterDropdown
+  }
+
+  selectFilter(option : any){
+    this.selectedFilter = option;
+    this.filterDropdown = false;
+    this.onFilterChange()
+    }
 
    getDeptClass(department: string): string {
     switch (department) {
