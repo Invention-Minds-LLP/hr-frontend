8fc9277773106ff6cd22427b7b5b0fd38d413adf
keerthana <keerthana.m@iamneo.ai>
2025-12-12 18:19:15 +0530

    env-update

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: false,
-    apiUrl:'https://hrminds-812956739285.us-east4.run.app/api',
+    // apiUrl:'https://hrminds-812956739285.us-east4.run.app/api',
     // apiUrl:'http://localhost:3002/api',
+     apiUrl:'http://192.168.3.25:3002/api'
   };
   
